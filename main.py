a2dbac5e9697fe0d1592ec6cd4e0b0470d231c7a
zuo-shi-yun <2818252801@qq.com>
2023-11-28 15:27:09 +0800

    init

--- main.py
+++ main.py
@@ -1,49 +1,34 @@
-from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
+from pkg.plugin.models import *
 
 """
-在收到私聊或群聊消息"hello"时，回复"hello, <发送者id>!"或"hello, everyone!"
+自动筛选、发送淘宝、京东优惠券
 """
 
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(Plugin):
-
-    # 插件加载时触发
-    # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
+@register(name="discountAssistant", description="自动筛选、发送淘宝、京东优惠券", version="1.0", author="zuoShiYun")
+class DiscountAssistant(Plugin):
     def __init__(self, plugin_host: PluginHost):
+        self.host = plugin_host
+
+    # 处理群、个人指令
+    @on(PersonNormalMessageReceived)
+    @on(GroupNormalMessageReceived)
+    def handle_normal_cmd(self, event: EventContext, **kwargs):
         pass
 
-    # 当收到个人消息时触发
-    @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
+    # 处理群、个人指令-cmd形式
+    @on(PersonCommandSent)
+    @on(GroupCommandSent)
+    def handle_cmd(self, event: EventContext, **kwargs):
+        pass
 
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
-
-    # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+    # 筛选优惠券
+    @on(GroupMessageReceived)
+    @on(PersonMessageReceived)
+    def group_normal_message_received(self, **kwargs):
+        pass
 
     # 插件卸载时触发
     def __del__(self):
